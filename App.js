ff8870c79296899ea16c715d373712ac5f54b43a
shanoysinc <shanoy96@gmail.com>
2020-06-08 23:27:21 -0500

    add icons to tab bar

--- App.js
+++ App.js
@@ -7,15 +7,43 @@ import Home from "./components/Home"
 import Progress from "./components/Progress"
 import AddHabits from "./components/AddHabits"
 import Settings from "./components/Settings"
-
+import Feather from "@expo/vector-icons/Feather"
 const Tab = createBottomTabNavigator()
 
 export default function App() {
 	return (
 		<NavigationContainer>
-			<Tab.Navigator>
+			<Tab.Navigator
+				screenOptions={({ route }) => ({
+					tabBarIcon: ({ color, size }) => {
+						let iconName
+
+						if (route.name == "Home") {
+							iconName = "home"
+						} else if (route.name == "Progress") {
+							iconName = "trending-up"
+						} else if (route.name == "AddHabits") {
+							iconName = "check-circle"
+						} else if (route.name == "Settings") {
+							iconName = "settings"
+						}
+
+						return (
+							<Feather
+								name={iconName}
+								size={size}
+								color={color}
+							/>
+						)
+					},
+				})}
+				tabBarOptions={{
+					activeTintColor: "purple",
+					inactiveTintColor: "gray",
+				}}
+			>
 				<Tab.Screen name="Home" component={Home} />
-				<Tab.Screen name="Progess" component={Progress} />
+				<Tab.Screen name="Progress" component={Progress} />
 				<Tab.Screen name="AddHabits" component={AddHabits} />
 				<Tab.Screen name="Settings" component={Settings} />
 			</Tab.Navigator>
