32d56c8069c3cfdcc57e285f28a15f7bde69ee9a
shanoysinc <shanoy96@gmail.com>
2020-06-10 10:03:11 -0500

    optimize memory usage

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import "react-native-gesture-handler"
+import { enableScreens } from "react-native-screens"
 import React from "react"
 import { StyleSheet, Text, View } from "react-native"
 import BottomTab from "./Tab/BottomTab"
 import { NavigationContainer } from "@react-navigation/native"
+
+enableScreens() // optimize memory usage and performance
 
 export default function App() {
 	return (
